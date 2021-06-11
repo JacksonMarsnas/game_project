4571335c46a44526f96030545051d2e9c8bbff1e
JacksonMarsnas <jmarsnas@gmail.com>
2021-06-11 15:50:32 -0700

    Make map into own class

--- main.lua
+++ main.lua
@@ -1,31 +1,12 @@
 function love.load()
+    Object = require "classic"
+    require "map"
+    require "map1"
     window_width = 960
     window_height = 960
     love.window.setMode(window_width, window_height)
 
-    grass = love.graphics.newImage("grass.png")
-    grass_frames = {}
-    tile_dimensions = 64
-    for i = 0, 1 do
-        table.insert(grass_frames, love.graphics.newQuad(i * tile_dimensions, 0, tile_dimensions, tile_dimensions, grass:getWidth(), grass:getHeight()))
-    end
-
-    tilemap = {{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
-    }
+    map1 = Map1()
 end
 
 function love.update()
@@ -33,13 +14,5 @@ function love.update()
 end
 
 function love.draw()
-    for column = 0, window_width / tile_dimensions do
-        for row = 0, window_height / tile_dimensions do
-            if (column % 2 == 0 and row % 2 == 1) or (column % 2 == 1 and row % 2 == 0) then
-                love.graphics.draw(grass, grass_frames[1], column * 64 - 64, row * 64 - 64)
-            elseif (column % 2 == 0 and row % 2 == 0) or (column % 2 == 1 and row % 2 == 1) then
-                love.graphics.draw(grass, grass_frames[2], column * 64 - 64, row * 64 - 64)
-            end
-        end
-    end
+    map1:draw()
 end
