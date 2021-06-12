68fafce6b1ecd2a394600c3f425cded721a0bc49
JacksonMarsnas <jmarsnas@gmail.com>
2021-06-12 15:40:15 -0700

    Add idle animations

--- character.lua
+++ character.lua
@@ -5,9 +5,9 @@ function Character:new()
     character_frames = {}
     sprite_dimensions = 64
 
-    for i = 0, 21 do
-        for j = 0, 13 do
-            table.insert(character_frames, love.graphics.newQuad(i * sprite_dimensions, j * sprite_dimensions, sprite_dimensions, sprite_dimensions, character_sheet:getWidth(), character_sheet:getHeight()))
+    for i = 0, 20 do
+        for j = 0, 12 do
+            table.insert(character_frames, love.graphics.newQuad(j * sprite_dimensions, i * sprite_dimensions, sprite_dimensions, sprite_dimensions, character_sheet:getWidth(), character_sheet:getHeight()))
         end
     end
 
@@ -16,13 +16,21 @@ function Character:new()
     self.y = 128
     self.current_x_tile = self.x / 64 + 1
     self.current_y_tile = self.y / 64 + 1
+    self.animations = self:create_animations()
+    self.current_frame = 1
+    self.speed_multiplier = 10
+    self.animation_state = "idle_down"
 end
 
 function Character:draw()
-    love.graphics.draw(character_sheet, character_frames[1], self.x, self.y)
+    love.graphics.draw(character_sheet, character_frames[self.animations[self.animation_state][math.floor(self.current_frame)]], self.x, self.y)
 end
 
 function Character:update(dt)
+    self.current_frame = self.current_frame + 1 * dt * self.speed_multiplier
+    if self.current_frame > #self.animations[self.animation_state] then
+        self.current_frame = 1
+    end
     if self.movement_animation ~= "none" then
         self:movement_animation(dt)
     end
@@ -74,4 +82,14 @@ function Character:movement_animation(dt)
             self.movement_direction = "none"
         end
     end
+end
+
+function Character:create_animations()
+    return {
+        idle_up = {1},
+        idle_left = {14},
+        idle_down = {27},
+        idle_right = {40},
+        walking_left = {118, 119, 120, 121, 122, 123, 124, 125, 126}
+    }
 end
