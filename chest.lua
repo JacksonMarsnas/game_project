1f828bc546bbc029c7e97dc1a0675dc20e88b305
JacksonMarsnas <jmarsnas@gmail.com>
2021-07-13 09:41:50 -0700

    Add feature so chests can only be looted once

--- chest.lua
+++ chest.lua
@@ -10,6 +10,7 @@ function Chest:loot()
     while move_unlocked == false do
         local new_move_index = math.random(#moves.all_moves)
         if moves.all_moves[new_move_index].locked == true then
+            all_maps[player.current_map].tilemap[player.current_y_tile][player.current_x_tile] = 10
             move_unlocked = true
             moves.all_moves[new_move_index].locked = false
         end
